46dd1dddff86a4d5d6b804a231bc819c27d91e48
kentsalcedo <kentsalcedo@Kents-MacBook-Pro.local>
2016-01-15 16:32:04 -1000

    Adds basic get handler and server is properly running

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-//test
+var express = require('express');
+var app = express();
+var router = express.Router();
+app.use('/', router);
+
+app.get('/', function(req,res,next){
+  res.send('hello world');
+});
+
+
+
+
+
+
+var server = app.listen(3000, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log(server.address());
+
+  console.log('Example app listening at localhost:', port);
+});
