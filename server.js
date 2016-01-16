d7c1ccb6a47df89ce069f2d847438b609c07b49d
kentsalcedo <kentsalcedo@Kents-MacBook-Pro.local>
2016-01-15 16:44:39 -1000

    Stubbed out get, post, put, delete handlers in server.js

--- server.js
+++ server.js
@@ -7,10 +7,32 @@ app.get('/', function(req,res,next){
   res.send('hello world');
 });
 
+app.route('/products')
+
+  .get(function(req,res){
+    res.send('get products');
+  })
+
+  .post(function(req,res){
+    res.send('post products');
+  })
+
+  .put(function(req,res){
+    res.send('put products');
+  })
+
+  .delete(function(req,res){
+    res.send('delete products');
+  });
 
 
+// router.use('/products', function(req,res,next){
+//   res.send('hello world');
+// });
 
-
+// router.use('/articles', function(req,res,next){
+//   res.send('hello world');
+// });
 
 var server = app.listen(3000, function(){
   var host = server.address().address;
