cb23303fc8eced0c5698d1ea4db4ba3610587205
kentsalcedo <kentsalcedo@Kents-MacBook-Pro.local>
2016-01-16 17:48:21 -1000

    Deletes commented out code

--- server.js
+++ server.js
@@ -12,7 +12,5 @@ var server = app.listen(3000, function(){
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log(server.address());
-
   console.log('Example app listening at localhost:', port);
 });
