39b5ab100e56e85141d6382f0591bafda433edd5
Chaz <cmtlum73@gmail.com>
2016-01-25 21:30:52 -1000

    comments out example of promise format

--- server.js
+++ server.js
@@ -7,7 +7,18 @@ var bodyParser = require('body-parser');
 var router = express.Router();
 var fs = require('fs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+// db.one( "insert into users( id, username, first_name, last_name ) values( default, $1, $2, $3 ) returning id", [ 'Chaz_Attack', 'Chaz', 'Lum' ] )
+//     .then(function ( data ) {
+//         // success;
+//         console.log( data.id, "Im inside you!!!" );
+//     })
+//     .catch(function (error) {
+//         // error;
+//         console.log( error, 'You done effed up' );
+//     });
+
+
+app.use( bodyParser.urlencoded( { extended: true } ) );
 
 app.use(methodOverride(function(req,res){
   var method = req.body._method;
@@ -44,6 +55,4 @@ app.set('views', './templates');
 var server = app.listen(3000, function(){
   var host = server.address().address;
   var port = server.address().port;
-
-  console.log('Example app listening at localhost:', port);
 });
